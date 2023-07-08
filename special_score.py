490e1f33cb33ce8d043fa33793cba186c08f2d05
Rathapol <Rathapol@cn333.com>
2023-07-08 22:48:31 +0700

    special_score

--- special_score.py
+++ special_score.py
@@ -3,3 +3,22 @@
 ข้อนี้ไม่ต้องทำก็ได้ ไม่มีผลลบกับการให้คะแนน แต่ถ้าทำมาเเล้วเจ๋งจริง ก็จะพิจารณาเป็นพิเศษ
 
 """
+
+#assume arr sort  ascending
+
+def binarySearch(arr,num):
+    minIndex = 0
+    maxIndex = len(arr)
+    while minIndex<=maxIndex:
+
+        midIndex = (minIndex + maxIndex)//2
+        if arr[midIndex] == num:
+            return midIndex
+        elif arr[midIndex]>num:
+            maxIndex = midIndex-1
+        elif arr[midIndex]<num:
+            minIndex = midIndex+1
+        
+    return -1
+
+print(binarySearch([1,2,3,4,5,6,7], 2))
